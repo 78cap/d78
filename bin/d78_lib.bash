4c05a14324f2a3c3e0a5f15f9e127719653fdc4f
Sandu Turcan <aturcan@seveneightcapital.com>
2022-05-09 15:56:44 -0400

    nucleus_client will display a message about --update initially

--- bin/d78_lib.bash
+++ bin/d78_lib.bash
@@ -10,13 +10,21 @@ function run_distro_cmd() {
     [ -d $DISTRO_DIR ] && rm -r $DISTRO_DIR
     [ $? != 0 ] && echo "Error occurred" && exit 1
     DISTRO_CMD=""
-  fi
-  if [ ! -e $DISTRO_DIR/venv/requirements.txt ]; then
     echo "Updating  $PROJECT_NAME distro in $DISTRO_DIR..."
     hg --cwd $SRC_DIR archive -r @ -t files $CLEAN_SRC_DIR -I settings.gradle -I gradle -I gradlew -I publics && \
     cd $CLEAN_SRC_DIR && \
     ./gradlew xargs-$PROJECT_NAME-distro --cmd "[distro_dir]/bin/post-install" && \
     echo "Done updating $DISTRO_DIR."
+    exit $?
+  fi
+  if [ ! -e $DISTRO_DIR/venv/requirements.txt ]; then
+    echo -e "This is a proxy script for \x1B[1m$PROJECT_NAME:$DISTRO_CMD\x1B[0m"
+    echo -e "To get the latest version, run:"
+    echo -e " - hg pull"
+    echo -e " - $(basename $0) --update"
+    echo -e "   Note: make sure your environment points to a compatible python install"
+    [ ! -z "$1" ] && exit 1
+    return
   fi
   export PATH=$DISTRO_DIR/bin:$PATH
   [ ! -z "$DISTRO_CMD" ] && exec $DISTRO_CMD "$@"
